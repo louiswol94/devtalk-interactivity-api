13ce1591ffdd5a83115590fc1c876caa1bff93cb
Louis Wolmarans <louiswol94@gmail.com>
2024-05-24 12:21:08 +0200

    step-2

--- src/view.js
+++ src/view.js
@@ -3,11 +3,6 @@
  */
 import { store, getContext } from '@wordpress/interactivity';
 
-function calculateStoryPoints( context ) {
-	let { sprints, ratio } = context;
-	context.storyPoints = Math.round( ( ( +sprints.sprint_1 + +sprints.sprint_2 + +sprints.sprint_3 ) / 3 ) * +ratio );
-}
-
 store( 'sp-calculator', {
 	actions: {
 		sprint_1: ( event ) => {

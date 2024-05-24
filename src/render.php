22fb5902fe076dd2387d7ec4bb17e85cc3c4e82e
Louis Wolmarans <louiswol94@gmail.com>
2024-05-24 11:53:25 +0200

    full code

--- src/render.php
+++ src/render.php
@@ -1,39 +1,48 @@
 <?php
-/**
- * PHP file to use when rendering the block type on the server to show on the front end.
- *
- * The following variables are exposed to the file:
- *     $attributes (array): The block attributes.
- *     $content (string): The block default content.
- *     $block (WP_Block): The block instance.
- *
- * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
- */
-
-// Generate unique id for aria-controls.
 $unique_id = wp_unique_id( 'p-' );
 ?>
 
 <div
 	<?php echo get_block_wrapper_attributes(); ?>
 	data-wp-interactive="sp-calculator"
-	<?php echo wp_interactivity_data_wp_context( array( 'isOpen' => false ) ); ?>
-	data-wp-watch="callbacks.logIsOpen"
+	<?php echo wp_interactivity_data_wp_context( [
+		'sprints' => [
+			'sprint_1' => 0,
+			'sprint_2' => 0,
+			'sprint_3' => 0
+		],
+		'ratio' => 1,
+		'storyPoints' => 0
+	] ); ?>
 >
-	<button
-		data-wp-on--click="actions.toggle"
-		data-wp-bind--aria-expanded="context.isOpen"
-		aria-controls="<?php echo esc_attr( $unique_id ); ?>"
-	>
-		<?php esc_html_e( 'Toggle', 'story-points-calculator' ); ?>
-	</button>
+	<div class="panel">
+		<p style="text-align: center;">Story points completed in the last 3 sprints:</p>
+		<div class="form-group">
+			<input type="number" placeholder="Sprint 1" data-wp-on--keyup="actions.sprint_1">
+		</div>
 
-	<p
-		id="<?php echo esc_attr( $unique_id ); ?>"
-		data-wp-bind--hidden="!context.isOpen"
-	>
-		<?php
-			esc_html_e( 'Story Points Calculator - hello from an interactive block!', 'story-points-calculator' );
-		?>
-	</p>
+		<div class="form-group">
+			<input type="number" placeholder="Sprint 2" data-wp-on--keyup="actions.sprint_2">
+		</div>
+
+		<div class="form-group">
+			<input type="number" placeholder="Sprint 3" data-wp-on--keyup="actions.sprint_3">
+		</div>
+
+		<div class="form-group">
+			<input type="number" max="1" step="0.1" min="0.1" placeholder="Ratio (0.1 - 1)" data-wp-on--keyup="actions.ratio">
+		</div>
+	</div>
+
+	<div class="panel">
+		<p class="mb-50">Here's how many Story Points you can take:</p>
+		<div class="normal-group width-full">
+			<div class="sps">
+				<span data-wp-text="context.storyPoints"></span>
+			</div>
+		</div>
+
+		<img src="<?php echo plugin_dir_url( dirname( __FILE__ ) ) . '/Elementor-Logo-Symbol-Pink.webp'; ?>" />
+		
+	</div>
 </div>
--- src/view.js
+++ src/view.js
@@ -3,18 +3,32 @@
  */
 import { store, getContext } from '@wordpress/interactivity';
 
+function calculateStoryPoints( context ) {
+	let { sprints, ratio } = context;
+	context.storyPoints = Math.round( ( ( +sprints.sprint_1 + +sprints.sprint_2 + +sprints.sprint_3 ) / 3 ) * +ratio );
+}
+
 store( 'sp-calculator', {
 	actions: {
-		toggle: () => {
+		sprint_1: ( event ) => {
 			const context = getContext();
-			context.isOpen = ! context.isOpen;
+			context.sprints.sprint_1 = event.target.value;
+			calculateStoryPoints( context );
 		},
-	},
-	callbacks: {
-		logIsOpen: () => {
-			const { isOpen } = getContext();
-			// Log the value of `isOpen` each time it changes.
-			console.log( `Is open?: ${ isOpen }` );
+		sprint_2: ( event ) => {
+			const context = getContext();
+			context.sprints.sprint_2 = event.target.value;
+			calculateStoryPoints( context );
 		},
-	},
+		sprint_3: ( event ) => {
+			const context = getContext();
+			context.sprints.sprint_3 = event.target.value;
+			calculateStoryPoints( context );
+		},
+		ratio: ( event ) => {
+			const context = getContext();
+			context.ratio = event.target.value;
+			calculateStoryPoints( context );
+		},
+	}
 } );
